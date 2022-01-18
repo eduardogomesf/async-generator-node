34212ecf3aba899173b55fffdd134f0b9ec04d7a
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-01-18 20:34:32 -0300

    feature: Creating stock API

---
+++ src/services/stock.js
@@ -0,0 +1,21 @@
+const { createServer } = require('http');
+
+const PORT = 3000;
+
+async function handler(request, response) {
+    if (
+        request.method === 'GET' &&
+        request.url.includes('products')
+    ) {
+        const { query: { productId } } = parse(request.url, true);
+
+        const stock = Math.floor(Math.random() * 100); // generate stock randomly
+
+        return response.end(JSON.stringify({ stock, productId }));
+    }
+
+    return response.end('Oops!')
+}
+
+createServer(handler)
+    .listen(PORT, () => console.log(`Stock API is running on ${PORT}`))
