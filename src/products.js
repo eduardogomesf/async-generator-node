42482077f5746b282437d3e0923fdc9cb9a5931a
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-01-18 20:34:13 -0300

    feature: Creating getProducts function

---
+++ src/products.js
@@ -0,0 +1,13 @@
+const { randomUUID } = require('crypto');
+
+const getProducts = async () => Array.from({ length: 1000 }, (_, index) => {
+    const basePrice = 150;
+
+    return {
+        id: randomUUID(),
+        name: `celular-${index}`,
+        price: index ? index * basePrice : basePrice,
+    }
+});
+
+module.exports = { getProducts }
